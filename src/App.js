0ed30ee91ab596e3853d3343d6e8538cd8315aa6
Chance Smith <chancesmithb@gmail.com>
2018-09-24 17:33:32 -0500

    📦  NEW: adds view toggle - WIP needs click tag to mark selected

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 import Textarea from './components/Textarea';
 import ScrapePreview from './components/ScrapePreview';
 
-const Styles = styled.div`
+const Styles = styled.div``;
+const Nav = styled.div``;
+const Views = styled.div`
   display: flex;
   width: 90vw;
   max-width: 90vw;
@@ -11,17 +13,33 @@ const Styles = styled.div`
   & > div {
     margin: 30px;
   }
+  & > div > div {
+    display: flex;
+  }
   .list {
     border: 1px solid #ccc;
     width: 20%;
+    li {
+      &.selected {
+        background: green;
+        color: white;
+        padding: 4px 5px;
+      }
+    }
   }
 `;
 
 class App extends Component {
   state = {
+    isViewInput: true,
     text:
       'An attack on the information systems of Hancock Health was initiated by an as-yet unidentified criminal group.  The attack used ransomware, a kind of computer malware that locks up computers until a ransom is paid, usually in the form of Bitcoin.  Through the effective teamwork of the Hancock technology team, an expert technology consulting group, and our clinical team, Hancock was able to recover the use of its computers, and at this time, there is no evidence that any patient information was adversely affected.   Hancock is continuing to work with national law enforcement to learn more about the incident.  We plan to provide additional information to our community regarding this act soon.',
     notedText: [
+      {
+        id: 0,
+        text: 'An',
+        selected: false,
+      },
       {
         id: 1,
         text: 'Hancock',
@@ -31,42 +49,87 @@ class App extends Component {
   };
 
   handleTextChange = text => {
-    this.setState({text});
+    this.setState({
+      text,
+    });
   };
 
   handleTextSelected = (id, text, add = true) => {
+    console.log('state:', this.state.notedText, ' add:', add, id, text);
     if (add) {
-      const allNoted = this.state.notedText;
-      const newNoted = allNoted.push({
+      // add to notedText
+      const notedText = this.state.notedText;
+      notedText.push({
         id: id,
         text: text,
+        selected: true,
       });
-      this.setState({notedText: newNoted});
+      console.log('add', notedText, text);
+      this.setState({notedText});
+    } else {
+      const notedText = this.state.notedText.map(item => {
+        // console.log(item, item.text, text, item.text === text, {...item, selected: false});
+        if (item.text === text) return {...item, selected: false};
+        return item;
+      });
+      console.log('after remove', notedText, text);
+      // remove from notedText
+      this.setState({notedText});
     }
-    console.log('text removed from array', text);
-    return true;
+  };
+
+  collectNotedWords = (text, pattern) => {
+    const splitText = text.split(pattern);
+    if (splitText.length <= 1) {
+      return text;
+    }
+
+    const matches = text.match(pattern);
+    const uniqueMatches = [...new Set(matches)]; // ES6 remove duplicates from array
+    return uniqueMatches.map((match, index) => ({id: index, text: match, selected: false}));
+  };
+
+  toggleView = () => {
+    this.setState({isViewInput: !this.state.isViewInput});
+
+    const notedText = this.collectNotedWords(this.state.text, /[A-Z].*?\b/g);
+    console.log(this.state.isViewInput, notedText);
+    this.setState({notedText});
   };
 
   render() {
     return (
       <Styles>
-        <Textarea textValue={this.state.text} handleTextChange={this.handleTextChange} />
-        <ScrapePreview
-          textValue={this.state.text}
-          selected={this.state.notedText.map(el => {
-            if (el.selected) return true;
-          })}
-          handleTextSelected={this.handleTextSelected}
-        />
-        <div className="list">
-          <ul>
-            {this.state.notedText.map((el, index) => {
-              if (el.selected) {
-                return <li key={index}>{el.text}</li>;
-              }
-            })}
-          </ul>
-        </div>
+        <Nav>
+          <button onClick={this.toggleView}>{this.state.isViewInput ? 'Next' : 'Back'}</button>
+        </Nav>
+        <Views>
+          {this.state.isViewInput ? (
+            <Textarea textValue={this.state.text} handleTextChange={this.handleTextChange} />
+          ) : (
+            <div>
+              <ScrapePreview
+                textValue={this.state.text}
+                notedText={this.state.notedText}
+                handleTextSelected={this.handleTextSelected}
+              />
+              <div className="list">
+                <ul>
+                  {this.state.notedText.map((el, index) => {
+                    if (el.selected) {
+                      return (
+                        <li className="selected" key={index}>
+                          {el.text}
+                        </li>
+                      );
+                    }
+                    return <li key={index}>{el.text}</li>;
+                  })}
+                </ul>
+              </div>
+            </div>
+          )}
+        </Views>
       </Styles>
     );
   }
--- src/components/ScrapePreview.js
+++ src/components/ScrapePreview.js
@@ -10,18 +10,29 @@ const Styles = styled.div`
   padding: 12px 15px;
   line-height: 1.5em;
   user-select: none;
+`;
 
+const Mark = styled.span`
   mark {
-    border: 1px solid #444;
     padding: 0 5px;
-    background: none;
+    background: ${props => {
+      if (props.selected) return 'green';
+      return 'none';
+    }};
+    color: ${props => {
+      if (props.selected) return 'white';
+      return 'inherit';
+    }};
+    border: ${props => {
+      if (props.selected) return '1px solid green';
+      return '1px solid #444';
+    }};
     cursor: pointer;
   }
 `;
 
 class ScrapePreview extends Component {
   handleMarkedClick = (id, text) => {
-    console.log('clicked', id, text);
     this.props.handleTextSelected(id, text);
   };
 
@@ -32,25 +43,33 @@ class ScrapePreview extends Component {
     }
 
     const matches = text.match(pattern);
-    return splitText.reduce(
-      (arr, element, index) =>
-        matches[index]
-          ? [
-              ...arr,
-              element,
-              <mark key={index} onClick={() => onClick(index, matches[index])}>
-                {matches[index]}
-              </mark>,
-            ]
-          : [...arr, element],
-      [],
-    );
+    return splitText.reduce((arr, element, index) => {
+      const isSelected = this.props.notedText.filter(obj => {
+        return obj.selected;
+      }).length;
+
+      console.log('selected', isSelected);
+
+      // no match
+      if (!matches[index]) return [...arr, element];
+      return [
+        ...arr,
+        element,
+        <Mark key={index} selected={isSelected} onClick={() => onClick(index, matches[index], true)}>
+          <mark>{matches[index]}</mark>
+        </Mark>,
+      ];
+    }, []);
   };
 
   render() {
     const cleanText = this.props.textValue.replace(/\n$/g, '\n\n');
     const marked = this.markText(cleanText, /[A-Z].*?\b/g, this.handleMarkedClick);
-    return <Styles>{marked}</Styles>;
+    return (
+      <Styles>
+        <div>{marked}</div>
+      </Styles>
+    );
   }
 }
 
